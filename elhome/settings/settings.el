bb83e896c39a56cf3a0872d1ffe513eb9aace196
Graham Hughes <graham@rethinkdb.com>
2015-01-05 13:30:52 -0800

    Add new colors.

--- elhome/settings/settings.el
+++ elhome/settings/settings.el
@@ -267,6 +267,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "White" :foreground "Black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 140 :width normal :foundry "nil" :family "Menlo"))))
  '(autoface-default ((t (:inherit default))) t)
  '(emacs-lisp-mode-default ((t (:inherit autoface-default :height 120 :family "Inconsolata"))) t))
 
