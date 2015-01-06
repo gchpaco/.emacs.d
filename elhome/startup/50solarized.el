0210f0af49c2efecf7e5433496cd0f7b2c3a0372
Graham Hughes <graham@sigwinch.org>
2015-01-06 01:00:29 -0800

    Use solarized, DAMMIT

--- elhome/startup/50solarized.el
+++ elhome/startup/50solarized.el
@@ -1,2 +1,5 @@
 ;;(require 'solarized-dark-theme)
-;;(require 'solarized-light-theme)
+(require 'solarized-light-theme)
+
+(add-to-list 'default-frame-alist '(foreground-color . "Black"))
+(add-to-list 'default-frame-alist '(background-color . "#fdf6e3"))
