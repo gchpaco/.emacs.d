6c97497160a06b619ab7dd746cba673d8f46578d
Graham Hughes <graham@rethinkdb.com>
2015-03-04 20:51:49 -0800

    Defined proper RethinkDB C++ headers.

--- elhome/startup/50c.el
+++ elhome/startup/50c.el
@@ -34,3 +34,55 @@
 
 (setq gdb-many-windows t)
 (setq gdb-show-main t)
+
+(defun my-get-project-relative-name ()
+  (file-relative-name (buffer-file-name)
+                      (concat (projectile-project-root) "src")))
+
+(defun my-alternate-file (name)
+  (concat (file-name-sans-extension (name)) ".hpp"))
+
+(define-skeleton my-cc-module-default
+  "C++ implementation file contents, per RethinkDB"
+  (my-alternate-file (my-get-project-relative-name))
+  "// Copyright " (format-time-string "%Y")
+  " RethinkDB, all rights reserved." \n
+  "#include \"" str "\"" \n \n
+  "// XXX C++ headers" \n
+  _ \n
+  "// XXX C headers" \n
+  _ \n
+  "#include \"errors.hpp\"" \n
+  "// XXX boost" \n
+  _ \n
+  "// XXX private headers" \n
+  _ \n
+  -)
+
+(add-to-list 'auto-insert-alist
+             '(("\\.cc" . "C++ file") . my-cc-module-default))
+
+(define-skeleton my-cc-header-default
+  "C++ interface file contents, per RethinkDB"
+  (upcase
+   (replace-regexp-in-string "[^A-Za-z]" "_" (my-get-project-relative-name)))
+  "// Copyright " (format-time-string "%Y")
+  " RethinkDB, all rights reserved." \n
+  "#ifndef " str "_" \n
+  "#define " str "_" \n
+  \n
+  "// XXX C++ headers" \n
+  _ \n
+  "// XXX C headers" \n
+  _ \n
+  "#include \"errors.hpp\"" \n
+  "// XXX boost" \n
+  _ \n
+  "// XXX private headers" \n
+  _ \n
+  - \n
+  \n
+  "#endif  // " str "_" \n)
+
+(add-to-list 'auto-insert-alist
+             '(("\\.hpp" . "C++ interface file") . my-cc-header-default))
