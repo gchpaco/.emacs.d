d0b9ff6d927d0645b88dcaceb27ad5648e90a76a
Graham Hughes <ghughes@meteor.com>
2015-07-02 11:53:30 -0700

    Delete rethinkdb project.

--- elhome/startup/50ede.el
+++ elhome/startup/50ede.el
@@ -1,10 +1,4 @@
 (require 'ede)
 (require 'ede/cpp-root)
 
-(ede-cpp-root-project "rethinkdb"
-                      :file (expand-file-name "~/wd/rethinkdb/config.mk")
-                      :include-path '("/external/gtest_1.6.0/include"
-                                      "/external/re2_20140111"
-                                      "/external/v8_3.22.24.17/include"
-                                      "/src")
-                      :system-include-path '("/usr/include"))
+
