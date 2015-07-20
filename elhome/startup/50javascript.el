b0f573766d59fd22a54c6b376d87c00ff9429127
Graham Hughes <ghughes@meteor.com>
2015-07-20 13:19:41 -0700

    Load js refactor mode

--- elhome/startup/50javascript.el
+++ elhome/startup/50javascript.el
@@ -1,3 +1,5 @@
+(require 'js2-refactor)
+
 (eval-after-load 'tern
   '(progn
      (require 'tern-auto-complete)
@@ -5,3 +7,5 @@
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+(add-hook 'js2-mode-hook #'js2-refactor-mode)
+(js2r-add-keybindings-with-prefix "C-c C-m")
