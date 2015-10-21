cdf6d258973b5d53b7303ecfa4e64fedaadc95e6
Graham Hughes <ghughes@meteor.com>
2015-10-21 15:25:45 -0700

    Enforce gofmt and get paths right.

--- elhome/startup/50go.el
+++ elhome/startup/50go.el
@@ -4,3 +4,6 @@
                              (whitespace-mode 0)))
 
 (setenv "GOPATH" "/Users/ghughes/lib/go:/Users/ghughes/wd/git-work/lib/go")
+(add-hook 'before-save-hook #'gofmt-before-save)
+(add-to-list 'exec-path "/usr/local/go/bin")
+(setenv "PATH" (concat (getenv "PATH") ":" "/usr/local/go/bin"))
