12b2bc1ae0670c619c93b89707206feac3ee93ed
Graham Hughes <graham@cloudvelox.com>
2016-06-20 17:03:49 -0700

    Don't need these with dabrahams's patch to elhome

--- elhome/site-lisp/system-name-cunningham-theme.el
+++
@@ -1,4 +0,0 @@
-(deftheme system-name-cunningham
-  "Created 2016-01-28.")
-
-(provide-theme 'system-name-cunningham)
--- elhome/site-lisp/system-name-cunningham.local-theme.el
+++
@@ -1,4 +0,0 @@
-(deftheme system-name-cunningham.local
-  "Created 2016-01-28.")
-
-(provide-theme 'system-name-cunningham.local)
