0805ea869a84260733e1c10a4cf0bf6b032fd9ef
Graham Hughes <graham@cloudvelox.com>
2016-08-16 14:38:31 -0700

    Handle Mac's stupid ls.

--- elhome/startup/50miscmodes.el
+++ elhome/startup/50miscmodes.el
@@ -1,6 +1,9 @@
 (eval-when-compile
   (require 'use-package))
 
+(when (eq system-type 'darwin)
+ (setq-default insert-directory-program "/usr/local/bin/gls"))
+
 (use-package electric-pair-mode
   :commands electric-pair-mode
   :init (add-hook 'prog-mode-hook 'electric-pair-mode))
