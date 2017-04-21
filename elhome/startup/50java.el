f402afe5fd935c7c744f7ff49c64f3628c95aae0
Graham Hughes <graham.hughes@farmersedge.ca>
2017-04-21 16:42:39 -0500

    Turn off the Java stuff for now.

--- elhome/startup/50java.el
+++ elhome/startup/50java.el
@@ -3,13 +3,16 @@
 
 (use-package emacs-eclim
   :ensure t
+  :disabled t
   :init
   (setq-default eclim-eclipse-dirs '("/Applications/Eclipse.app/Contents/Eclipse/")
                 eclim-executable "/Applications/Eclipse.app/Contents/Eclipse/eclimd")
   (add-hook 'after-init-hook 'global-eclim-mode))
 
-(use-package eclimd)
+(use-package eclimd
+  :disabled t)
 
 (use-package company-emacs-eclim
   :functions company-emacs-eclim-setup
-  :config (company-emacs-eclim-setup))
+  :config (company-emacs-eclim-setup)
+  :disabled t)
