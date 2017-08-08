6e3e612e48168b964fa2f33438b745c5e3802510
Graham Hughes <graham@sigwinch.org>
2017-08-08 18:02:53 -0500

    Start using straight.el

--- init.el
+++ init.el
@@ -10,6 +10,19 @@
 
 (setq inhibit-startup-screen t)
 
+(let ((bootstrap-file (concat user-emacs-directory "straight/bootstrap.el"))
+      (bootstrap-version 2))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
 (defvar package--builtin-versions
   ;; Mostly populated by loaddefs.el via autoload-builtin-package-versions.
   (purecopy `((emacs . ,(version-to-list emacs-version))))
---
+++ straight/versions/default.el
@@ -0,0 +1,6 @@
+(("diminish.el" . "d5c61a14e1a5590a65f83c099a5bd42fcadff24d")
+ ("epkgs" . "a805077922499ab878d8b9b8f6b60a92935918cd")
+ ("melpa" . "6ef473594ec57a747ad7d9d57d7287bcacf4b446")
+ ("straight.el" . "5aad7999ab7d3682cf061d718286ae27837639a4")
+ ("use-package" . "7b055494e39efba8b237202b7c97f40aa19e2579"))
+:venus
