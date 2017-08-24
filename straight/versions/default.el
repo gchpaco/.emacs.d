58f3bb7f8eaaec9dd430f2ec65cc182bf5e2aaf3
Graham Hughes <graham@sigwinch.org>
2017-08-24 16:07:08 -0500

    Update straight.el

--- straight/versions/default.el
+++ straight/versions/default.el
@@ -1,6 +1,6 @@
 (("diminish.el" . "d5c61a14e1a5590a65f83c099a5bd42fcadff24d")
  ("epkgs" . "a805077922499ab878d8b9b8f6b60a92935918cd")
- ("melpa" . "6ef473594ec57a747ad7d9d57d7287bcacf4b446")
- ("straight.el" . "5aad7999ab7d3682cf061d718286ae27837639a4")
- ("use-package" . "7b055494e39efba8b237202b7c97f40aa19e2579"))
+ ("melpa" . "c97d3f653057eab35c612109792884334be556fe")
+ ("straight.el" . "9a998d766d64fbfb790f67f28e63b0f3fd9b0119")
+ ("use-package" . "360df30683a711c443f87e495ba14cdd125a505d"))
 :venus
