ded73b185f1120940b5bfd446ef539467852b340
Graham Hughes <graham@sigwinch.org>
2021-05-28 12:24:52 -0500

    Straighten out LSP configuration a bit

--- elhome/startup/50lsp.el
+++ elhome/startup/50lsp.el
@@ -5,17 +5,9 @@
 (use-package company-lsp :straight t)
 (use-package helm-lsp :straight t)
 (use-package lsp-ui :straight t)
-(use-package lsp-treemacs :straight t)
 (use-package lsp-java
   :straight t
   :after lsp
   :config
   (add-hook 'java-mode-hook #'lsp))
-(use-package dap-mode
-  :straight t
-  :after lsp-mode
-  :config
-  (dap-mode t)
-  (dap-ui-mode t))
-(use-package dap-java :after lsp-java :disabled t)
-(use-package lsp-java-treemacs :after treemacs :disabled t)
+(use-package lsp-latex :straight t)
